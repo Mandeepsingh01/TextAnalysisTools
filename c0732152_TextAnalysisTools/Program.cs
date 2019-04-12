3f097ed5c4f32040c407bd3c31ca86a1c215aff7
visualstudio <visualstudio@122-PC34>
2019-04-12 19:35:34 -0400

    Add project files.

---
+++ c0732152_TextAnalysisTools/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c0732152_TextAnalysisTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
