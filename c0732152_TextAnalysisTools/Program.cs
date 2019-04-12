75428bdc388e1e5d619d8574c6f6c4cfe9a5fb13
visualstudio <visualstudio@122-PC34>
2019-04-12 19:41:23 -0400

    final

--- c0732152_TextAnalysisTools/Program.cs
+++ c0732152_TextAnalysisTools/Program.cs
@@ -8,8 +8,38 @@ namespace c0732152_TextAnalysisTools
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+       
+            static void Main(string[] args)
+            {
+
+                Program a = new Program();
+
+                Console.WriteLine(a.MyfunctionA("Ada Lovelace wrote the first algorithm designed for processing by an Analytical engine"));
+                Console.ReadLine();
+
+            }
+            public string MyfunctionA(string input)
+            {
+                string[] words = input.Split(' ');
+                int wordArraylength = words.Length;
+                int[] wordsLength = new int[wordArraylength];
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    wordsLength[i] = words[i].Length;
+                }
+
+                string currentWord = words[0];
+                string nextWord;
+
+                for (int y = 0; y < words.Length - 1; y++)
+                {
+                    currentWord = words[y];
+                    nextWord = words[y + 1];
+                }
+                return currentWord;
+            }
         }
     }
-}
+
+       
